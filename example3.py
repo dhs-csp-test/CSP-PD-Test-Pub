c179b1c062fa36781b6edd6c88143f0dbe34a852
dhs-csp-test <adam.brown.engineering@gmail.com>
2020-02-09 05:01:23 +0000

    Explain the work you and your partner did.......

--- example3.py
+++ example3.py
@@ -23,7 +23,7 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b' for collude or betray.
     '''
     if len(my_history)==0: # It's the first round; collude.
-        return 'c'
+        return 'c' 
     elif my_history[-1]=='c' and their_history[-1]=='b':
         return 'b' # Betray if they were severely punished last time,
     else:
